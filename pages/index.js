a4a6c377e819aa75fd8ad10ba53b71e42b74782a
itc-s23014 <s23014@std.it-college.ac.jp>
2023-12-14 00:34:50 +0000

    index.js_push
    index_プシュ

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
+import Header from '../components/header'
+import Hero from '../components/hero'
+import Footer from '../components/footer'
 const Home = () => {
   return (
     <>
-      <header>HEADER</header>
+     <Header />
       <main>
-        <div>
-          <h1>CUBE</h1>
-          <p>アウトプットしていくサイト</p>
-        </div>
-      </main>
-      <footer>FOOTER</footer>
+        <Hero />
+    </main>
+    <Footer />
     </>
   )
 }

7f49ba13cbd6b536c7952d7041636d3ed7394476
itc-s23014 <s23014@std.it-college.ac.jp>
2023-12-14 00:43:25 +0000

    Revert "index.js_push"
    This reverts commit a4a6c377e819aa75fd8ad10ba53b71e42b74782a.

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
-import Header from '../components/header'
-import Hero from '../components/hero'
-import Footer from '../components/footer'
 const Home = () => {
   return (
     <>
-     <Header />
+      <header>HEADER</header>
       <main>
-        <Hero />
-    </main>
-    <Footer />
+        <div>
+          <h1>CUBE</h1>
+          <p>アウトプットしていくサイト</p>
+        </div>
+      </main>
+      <footer>FOOTER</footer>
     </>
   )
 }

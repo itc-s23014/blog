439de621dc14c4aefda26478b50c7ec99f8419fe
itc-s23014 <s23014@std.it-college.ac.jp>
2023-12-19 07:20:00 +0000

    3.5と3.6のスタイル適用

--- components/hero.js
+++ components/hero.js
@@ -1,10 +1,14 @@
-const Hero = props => {
+import styles from 'styles/hero.module.css'
+const Hero = ({ title, subtitle, imageOn = false }) => {
   return (
     <div>
-      <h1>{props.title}</h1>
-      <p>{props.subtitle}</p>
-      {props.imageOn && <figure> [画像] </figure>}
+      <div className={styles.text}>
+        <h1 className={styles.title}>{title}</h1>
+        <p className={styles.subtitle}>{subtitle}</p>
+      </div>
+      {imageOn && <figure>[画像]</figure>}
     </div>
   )
 }
+
 export default Hero
